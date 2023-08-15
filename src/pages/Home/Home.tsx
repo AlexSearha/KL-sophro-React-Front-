2ffd9da6746dc61f5da50cc81265e85a732c7079
AlexSearha <contact@mytechcompanion.fr>
2023-08-15 14:05:31 +0200

    init dependances packageJson

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import './style.scss';
+
+function Home() {
+  return <h2>Coucou</h2>;
+}
+
+export default Home;
---
+++ src/pages/MyAccount/MyAccount.tsx
@@ -0,0 +1,5 @@
+function MyAcccount() {
+  return <h2>Mon compte</h2>;
+}
+
+export default MyAcccount;
