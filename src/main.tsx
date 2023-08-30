0fcb1cc6e4e78abd5f723fd64d8909cbe69a3ee2
AlexSearha <contact@mytechcompanion.fr>
2023-08-30 09:26:13 +0200

    ajout du provider store dans le main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './components/App/App';
+import store from './store';
 
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
